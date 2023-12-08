7b2c0bd1cf99e5b0dfc9a85b2b07d496ce29fdfb
Leo Genuardi <leo.genuardi@lairdconnect.com>
2023-12-08 15:11:10 +0000

    Change the json/yaml interpreter into a static library
    PROD-4273

--- common_lib/json_yaml_interpreter.py
+++ common_lib/json_yaml_interpreter.py
@@ -1,26 +1,22 @@
 import json
 import yaml
 
-class JsonYamlInterpreter:
-    def __init__(self, file_path):
-        self.file_path = file_path
-
-    # Reads data into a python dictionary format based upon the file type.
-    # Dictionary format is the same regardless of the file format.
-    def read_data(self):
-        try:
-            with open(self.file_path, 'r') as file:
-                if self.file_path.endswith('.json'):
-                    data = json.load(file)
-                elif self.file_path.endswith('.yaml') or self.file_path.endswith('.yml'):
-                    data = yaml.safe_load(file)
-                else:
-                    print("Unsupported file format.")
-                    return None
-            return data
-        except FileNotFoundError:
-            print("File not found.")
-            return None
+# Reads data into a python dictionary format based upon the file type.
+# Dictionary format is the same regardless of the file format.
+def read_data(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            if file_path.endswith('.json'):
+                data = json.load(file)
+            elif file_path.endswith('.yaml') or file_path.endswith('.yml'):
+                data = yaml.safe_load(file)
+            else:
+                print("Unsupported file format.")
+                return None
+        return data
+    except FileNotFoundError:
+        print("File not found.")
+        return None
 
 
 
